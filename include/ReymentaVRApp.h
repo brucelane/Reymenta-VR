cba2d50dd493a3ab036baa51565aed0601d28918
Bruce LANE <bruce@batchass.fr>
2015-12-07 00:03:04 +0100

    imgui assets in another project

--- include/ReymentaVRApp.h
+++ include/ReymentaVRApp.h
@@ -1,21 +1,42 @@
 
+
+#pragma warning(push)
+#pragma warning(disable: 4996) // _CRT_SECURE_NO_WARNINGS
+
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 #include "cinder/gl/Texture.h"
 #include "cinder/Log.h"
 #include "cinder/Utilities.h"
+#include "cinder/app/Platform.h"
+#include "cinder/gl/Environment.h"
+#include "cinder/gl/Fbo.h"
+#include "cinder/gl/GlslProg.h"
+#include "cinder/ImageIo.h"
+#include "cinder/Unicode.h"
 
 #include "CinderOculus.h"
 
+// UserInterface
+#include "CinderImGui.h"
+// parameters
+#include "ParameterBag.h"
+// audio
+#include "AudioWrapper.h"
+// spout
+#include "SpoutWrapper.h"
+// Utils
+#include "Batchass.h"
+// Console
+#include "AppConsole.h"
 
-// -------- SPOUT -------------
-#include "Spout.h"
-// ----------------------------
+#include <time.h>
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
+using namespace Reymenta;
 
 class ReymentaVRApp : public App {
 
@@ -23,22 +44,23 @@ public:
 	ReymentaVRApp();
 
 	void setup() override;
+	void cleanup() override;
 	void draw() override;
 	void update() override;
 	void mouseDown(MouseEvent event) override;
-	void keyDown(KeyEvent event) override;
+	void mouseDrag(MouseEvent event) override;
+	void mouseUp(MouseEvent event) override;
+	void mouseMove(MouseEvent event) override;
 
-	// -------- SPOUT -------------
-	SpoutReceiver spoutreceiver;                // Create a Spout receiver object
-	void shutdown();
-	bool bInitialized;                          // true if a sender initializes OK
-	bool bDoneOnce;                             // only try to initialize once
-	bool bMemoryMode;                           // tells us if texture share compatible
-	unsigned int g_Width, g_Height;             // size of the texture being sent out
-	char SenderName[256];                       // sender name 
-	gl::TextureRef spoutTexture;                    // Local Cinder texture used for sharing
-	// ----------------------------
+	void keyDown(KeyEvent event) override;
+	void keyUp(KeyEvent event) override;
+
+	void resize() override;
+	void fileDrop(FileDropEvent event) override;
+
 private:
+	void saveThumb();
+
 	void drawPositionalTrackingCamera() const;
 	double			mTime;
 
@@ -48,4 +70,57 @@ private:
 	gl::BatchRef	mTeapot;
 
 	vec4			mLightWorldPosition;
+	//! Sets the ShaderToy uniform variables.
+	void setUniforms();
+	//! Shader that will perform the transition to the next shader.
+	gl::GlslProgRef mShaderTransition;
+	//! Buffer containing the rendered output of the currently active shader.
+	gl::FboRef      mBufferCurrent;
+	//! Buffer containing the rendered output of the shader we are transitioning to.
+	gl::FboRef      mBufferNext;
+	//! Texture slots for our shader, based on ShaderToy.
+	gl::TextureRef  mChannel0;
+	gl::TextureRef  mChannel1;
+	gl::TextureRef  mChannel2;
+	gl::TextureRef  mChannel3;
+	//! Our mouse position: xy = current position while mouse down, zw = last click position.
+	vec4            mMouse;
+
+	// parameters
+	ParameterBagRef				mParameterBag;
+	// audio
+	AudioWrapperRef				mAudio;
+	// utils
+	BatchassRef					mBatchass;
+	// console
+	AppConsoleRef				mConsole;
+	// spout
+	SpoutWrapperRef				mSpout;
+	// timeline
+	Anim<float>					mTimer;
+
+	// imgui
+	float						color[4];
+	float						backcolor[4];
+	int							playheadPositions[12];
+	float						speeds[12];
+	int							w;
+	int							h;
+	int							displayHeight;
+	int							xPos;
+	int							yPos;
+	int							largeW;
+	int							largeH;
+	int							largePreviewW;
+	int							largePreviewH;
+	int							margin;
+	int							inBetween;
+	//bool						sNewFrame;
+	float						f = 0.0f;
+	char						buf[64];
+	bool						showConsole, showGlobal, showTextures, showTest, showMidi, showFbos, showTheme, showAudio, showShaders, showOSC, showChannels;
+	bool						mouseGlobal;
+	void						ShowAppConsole(bool* opened);
+	// log only the first time
+	bool						mFirstLaunch;
 };
--- src/ReymentaVRApp.cpp
+++ src/ReymentaVRApp.cpp
@@ -24,63 +24,150 @@ ReymentaVRApp::ReymentaVRApp()
 	gl::enable(GL_CLIP_DISTANCE0, true);
 }
 
-// -------- SPOUT -------------
 void ReymentaVRApp::setup()
 {
-    g_Width = 320; // set global width and height to something
-    g_Height = 240; // they need to be reset when the receiver connects to a sender
-    //setWindowSize(g_Width, g_Height);
-    setFullScreen(false);
-    setFrameRate(60.0f);
-    bInitialized = false;
+	int wr;
+	// parameters
+	mParameterBag = ParameterBag::create();
+	mParameterBag->mLiveCode = true;
+	mParameterBag->mRenderThumbs = true;
+	// utils
+	mBatchass = Batchass::create(mParameterBag);
+	CI_LOG_V("reymenta setup");
+	mFirstLaunch = true;
+	wr = mBatchass->getWindowsResolution();
+	//setWindowSize(mParameterBag->mMainWindowWidth, mParameterBag->mMainWindowHeight);
+	// setup shaders and textures
+	mBatchass->setup();
+	//setWindowPos(ivec2(0, 0));
+	mParameterBag->iResolution.x = mParameterBag->mRenderWidth;
+	mParameterBag->iResolution.y = mParameterBag->mRenderHeight;
+	mParameterBag->mRenderResolution = ivec2(mParameterBag->mRenderWidth, mParameterBag->mRenderHeight);
+	mParameterBag->mRenderResoXY = vec2(mParameterBag->mRenderWidth, mParameterBag->mRenderHeight);
+	mParameterBag->mRenderPosXY = ivec2(mParameterBag->mRenderX, mParameterBag->mRenderY);
+	mParameterBag->mMode = 0;
+
+	// Load our textures and transition shader in the main thread.
+	try {
+		gl::Texture::Format fmt;
+		fmt.setWrap(GL_REPEAT, GL_REPEAT);
+
+		mChannel0 = gl::Texture::create(loadImage(loadAsset("presets/tex16.png")), fmt);
+		mChannel1 = gl::Texture::create(loadImage(loadAsset("presets/tex06.jpg")), fmt);
+		mChannel2 = gl::Texture::create(loadImage(loadAsset("presets/tex09.jpg")), fmt);
+		mChannel3 = gl::Texture::create(loadImage(loadAsset("presets/tex02.jpg")), fmt);
+
+		mShaderTransition = gl::GlslProg::create(loadAsset("common/shadertoy.vert"), loadAsset("common/shadertoy.frag"));
+	}
+	catch (const std::exception& e) {
+		// Quit if anything went wrong.
+		CI_LOG_EXCEPTION("Failed to load common textures and shaders:", e);
+		quit(); return;
+	}
+	// instanciate the audio class
+	mAudio = AudioWrapper::create(mParameterBag, mBatchass->getTexturesRef());
+	// instanciate the spout class
+	mSpout = SpoutWrapper::create(mParameterBag, mBatchass->getTexturesRef());
+	// instanciate the console class
+	mConsole = AppConsole::create(mParameterBag, mBatchass);
+
+	mTimer = 0.0f;
+
+	// imgui
+	margin = 3;
+	inBetween = 3;
+	// mPreviewFboWidth 80 mPreviewFboHeight 60 margin 10 inBetween 15 mPreviewWidth = 160;mPreviewHeight = 120;
+	w = mParameterBag->mPreviewFboWidth + margin;
+	h = mParameterBag->mPreviewFboHeight * 2.3;
+	largeW = (mParameterBag->mPreviewFboWidth + margin) * 4;
+	largeH = (mParameterBag->mPreviewFboHeight + margin) * 5;
+	largePreviewW = mParameterBag->mPreviewWidth + margin;
+	largePreviewH = (mParameterBag->mPreviewHeight + margin) * 2.4;
+	displayHeight = mParameterBag->mMainDisplayHeight - 50;
+	mouseGlobal = false;
+
+	showConsole = showGlobal = showAudio = showMidi = showChannels = showShaders = showOSC = true;
+	showTest = showTheme = showFbos = showTextures = false;
+
+	// set ui window and io events callbacks
+	ui::initialize();
+
+	// RTE mBatchass->getShadersRef()->setupLiveShader();
+	mBatchass->tapTempo();
 }
 
 void ReymentaVRApp::update()
 {
-    unsigned int width, height;
+	//mSpout->update();
+	//mBatchass->update();
+	mAudio->update();
+	mParameterBag->iFps = getAverageFps();
+	mParameterBag->sFps = toString(floor(mParameterBag->iFps));
+	getWindow()->setTitle(std::string("(" + mParameterBag->sFps + " fps)"));
+	// Bind textures.
+	if (mChannel0) mChannel0->bind(0);
+	if (mChannel1) mChannel1->bind(1);
+	if (mChannel2) mChannel2->bind(2);
+	if (mChannel3) mChannel3->bind(3);
 
-    // -------- SPOUT -------------
-    if (!bInitialized) {
+	// Render the current shader to a frame buffer.
+	if (mBatchass->getShadersRef()->getShaderCurrent() && mBufferCurrent) {
+		gl::ScopedFramebuffer fbo(mBufferCurrent);
 
-        // This is a receiver, so the initialization is a little more complex than a sender
-        // The receiver will attempt to connect to the name it is sent.
-        // Alternatively set the optional bUseActive flag to attempt to connect to the active sender. 
-        // If the sender name is not initialized it will attempt to find the active sender
-        // If the receiver does not find any senders the initialization will fail
-        // and "CreateReceiver" can be called repeatedly until a sender is found.
-        // "CreateReceiver" will update the passed name, and dimensions.
-        SenderName[0] = NULL; // the name will be filled when the receiver connects to a sender
-        width = g_Width; // pass the initial width and height (they will be adjusted if necessary)
-        height = g_Height;
+		// Bind shader.
+		gl::ScopedGlslProg shader(mBatchass->getShadersRef()->getShaderCurrent());
+		setUniforms();
 
-        // Optionally set for DirectX 9 instead of default DirectX 11 functions
-        // spoutreceiver.SetDX9(true);  
+		// Clear buffer and draw full screen quad (flipped).
+		gl::clear();
+		gl::drawSolidRect(Rectf(0, (float)getWindowHeight(), (float)getWindowWidth(), 0));
+	}
 
-        // Initialize a receiver
-        if (spoutreceiver.CreateReceiver(SenderName, width, height, true)) { // true to find the active sender
-            // Optionally test for texture share compatibility
-            // bMemoryMode informs us whether Spout initialized for texture share or memory share
-            bMemoryMode = spoutreceiver.GetMemoryShareMode();
+	// Render the next shader to a frame buffer.
+	if (mBatchass->getShadersRef()->getShaderNext() && mBufferNext) {
+		gl::ScopedFramebuffer fbo(mBufferNext);
 
-            // Is the size of the detected sender different from the current texture size ?
-            // This is detected for both texture share and memoryshare
-            if (width != g_Width || height != g_Height) {
-                // Reset the global width and height
-                g_Width = width;
-                g_Height = height;
-                // Reset the local receiving texture size
-                spoutTexture = gl::Texture::create(g_Width, g_Height);
-                // reset render window
-                setWindowSize(g_Width, g_Height);
-            }
-            bInitialized = true;
-        }
-        else {
-            // Receiver initialization will fail if no senders are running
-            // Keep trying until one starts
-        }
-    } // endif not initialized
-    // ----------------------------
+		// Bind shader.
+		gl::ScopedGlslProg shader(mBatchass->getShadersRef()->getShaderNext());
+		setUniforms();
+
+		// Clear buffer and draw full screen quad (flipped).
+		gl::clear();
+		gl::drawSolidRect(Rectf(0, (float)getWindowHeight(), (float)getWindowWidth(), 0));
+	}
+
+	// Perform a cross-fade between the two shaders.
+	double time = getElapsedSeconds() - mParameterBag->mTransitionTime;
+	double fade = math<double>::clamp(time / mParameterBag->mTransitionDuration, 0.0, 1.0);
+
+	if (fade <= 0.0) {
+		// Transition has not yet started. Keep drawing current buffer.
+		gl::draw(mBufferCurrent->getColorTexture(), getWindowBounds());
+	}
+	else if (fade < 1.0) {
+		// Transition is in progress.
+		// Use a transition shader to avoid having to draw one buffer on top of another.
+		gl::ScopedTextureBind tex0(mBufferCurrent->getColorTexture(), 0);
+		gl::ScopedTextureBind tex1(mBufferNext->getColorTexture(), 1);
+
+		gl::ScopedGlslProg shader(mShaderTransition);
+		mShaderTransition->uniform("iSrc", 0);
+		mShaderTransition->uniform("iDst", 1);
+		mShaderTransition->uniform("iFade", (float)fade);
+
+		gl::drawSolidRect(getWindowBounds());
+	}
+	else if (mBatchass->getShadersRef()->getShaderNext()) {
+		// Transition is done. Swap shaders.
+		gl::draw(mBufferNext->getColorTexture(), getWindowBounds());
+		mBatchass->getShadersRef()->swapShaders();
+
+	}
+	else {
+		// No transition in progress.
+		gl::draw(mBufferCurrent->getColorTexture(), getWindowBounds());
+	}
+	// rift
 	mTime = getElapsedSeconds();
 	gl::clear();
 
@@ -114,20 +201,53 @@ void ReymentaVRApp::update()
 		mShader->uniform("uWorldToEyeClipMatrices", worldToEyeClipMatrices.data(), 6);
 		mTeapot->drawInstanced(2);
 	}
-	getWindow()->setTitle(std::string("(" + toString(floor(getAverageFps())) + " fps)"));
+
 }
 // -------- SPOUT -------------
-void ReymentaVRApp::shutdown()
+void ReymentaVRApp::cleanup()
 {
-    spoutreceiver.ReleaseReceiver();
+	CI_LOG_V("shutdown");
+	// save warp settings
+	mBatchass->getWarpsRef()->save();
+	// save params
+	mParameterBag->save();
+	ui::Shutdown();
+	// close spout
+	mSpout->shutdown();
+	quit();
 }
-
+void ReymentaVRApp::mouseMove(MouseEvent event)
+{
+	if (mParameterBag->mMode == mParameterBag->MODE_WARP) mBatchass->getWarpsRef()->mouseMove(event);
+}
 void ReymentaVRApp::mouseDown(MouseEvent event)
 {
-    if (event.isRightDown()) { // Select a sender
-        // SpoutPanel.exe must be in the executable path
-        spoutreceiver.SelectSenderPanel(); // DirectX 11 by default
-    }
+	mMouse.x = (float)event.getPos().x;
+	mMouse.y = (float)event.getPos().y;
+	mMouse.z = (float)event.getPos().x;
+	mMouse.w = (float)event.getPos().y;
+	if (mParameterBag->mMode == mParameterBag->MODE_WARP) mBatchass->getWarpsRef()->mouseDown(event);
+	if (mParameterBag->mMode == mParameterBag->MODE_AUDIO) mAudio->mouseDown(event);
+
+}
+
+void ReymentaVRApp::mouseDrag(MouseEvent event)
+{
+	mMouse.x = (float)event.getPos().x;
+	mMouse.y = (float)event.getPos().y;
+	if (mParameterBag->mMode == mParameterBag->MODE_WARP) mBatchass->getWarpsRef()->mouseDrag(event);
+	if (mParameterBag->mMode == mParameterBag->MODE_AUDIO) mAudio->mouseDrag(event);
+
+}
+void ReymentaVRApp::mouseUp(MouseEvent event)
+{
+	if (mParameterBag->mMode == mParameterBag->MODE_WARP) mBatchass->getWarpsRef()->mouseUp(event);
+	if (mParameterBag->mMode == mParameterBag->MODE_AUDIO) mAudio->mouseUp(event);
+}
+void ReymentaVRApp::keyUp(KeyEvent event)
+{
+	if (mParameterBag->mMode == mParameterBag->MODE_WARP) mBatchass->getWarpsRef()->keyUp(event);
+
 }
 // ----------------------------
 void ReymentaVRApp::keyDown(KeyEvent event)
@@ -151,63 +271,215 @@ void ReymentaVRApp::keyDown(KeyEvent event)
 		break;
 	}
 }
+void ReymentaVRApp::resize()
+{
+	mBatchass->getWarpsRef()->resize();
+	// Create/resize frame buffers (no multisampling)
+	mBufferCurrent = gl::Fbo::create(getWindowWidth(), getWindowHeight());
+	mBufferNext = gl::Fbo::create(getWindowWidth(), getWindowHeight());
+}
 
+void ReymentaVRApp::fileDrop(FileDropEvent event)
+{
+	// Send all file requests to the loading thread.
+	size_t count = event.getNumFiles();
+	//for (size_t i = 0; i < count && mRequests->isNotFull(); ++i) {
+	//mRequests->pushFront(event.getFile(i));
+	for (size_t i = 0; i < count; ++i) {
+		mBatchass->getShadersRef()->addRequest(event.getFile(i));
+	}
+	// TO MIGRATE
+	int index;
+	string ext = "";
+	// use the last of the dropped files
+	const fs::path &mPath = event.getFile(event.getNumFiles() - 1);
+	string mFile = mPath.string();
+	int dotIndex = mFile.find_last_of(".");
+	int slashIndex = mFile.find_last_of("\\");
 
+	if (dotIndex != std::string::npos && dotIndex > slashIndex) ext = mFile.substr(mFile.find_last_of(".") + 1);
+	index = (int)(event.getX() / (margin + mParameterBag->mPreviewFboWidth + inBetween));// +1;
+	//mBatchass->log(mFile + " dropped, currentSelectedIndex:" + toString(mParameterBag->currentSelectedIndex) + " x: " + toString(event.getX()) + " PreviewFboWidth: " + toString(mParameterBag->mPreviewFboWidth));
+
+	if (ext == "wav" || ext == "mp3")
+	{
+		mAudio->loadWaveFile(mFile);
+	}
+	else if (ext == "png" || ext == "jpg")
+	{
+		if (index < 1) index = 1;
+		if (index > 3) index = 3;
+		//mTextures->loadImageFile(mParameterBag->currentSelectedIndex, mFile);
+		mBatchass->getTexturesRef()->loadImageFile(index, mFile);
+	}
+	else if (ext == "glsl")
+	{
+		if (index < 4) index = 4;
+		int rtn = mBatchass->getShadersRef()->loadPixelFragmentShaderAtIndex(mFile, index);
+		if (rtn > -1 && rtn < mBatchass->getShadersRef()->getCount())
+		{
+			mParameterBag->controlValues[22] = 1.0f;
+			// TODO  send content via websockets
+			/*fs::path fr = mFile;
+			string name = "unknown";
+			if (mFile.find_last_of("\\") != std::string::npos) name = mFile.substr(mFile.find_last_of("\\") + 1);
+			if (fs::exists(fr))
+			{
+
+			std::string fs = loadString(loadFile(mFile));
+			if (mParameterBag->mOSCEnabled) mOSC->sendOSCStringMessage("/fs", 0, fs, name);
+			}*/
+			// save thumb
+			timeline().apply(&mTimer, 1.0f, 1.0f).finishFn([&]{ saveThumb(); });
+		}
+	}
+	else if (ext == "mov" || ext == "mp4")
+	{
+		/*
+		if (index < 1) index = 1;
+		if (index > 3) index = 3;
+		mBatchass->getTexturesRef()->loadMovieFile(index, mFile);*/
+	}
+	else if (ext == "fs")
+	{
+		//mShaders->incrementPreviewIndex();
+		mBatchass->getShadersRef()->loadFragmentShader(mPath);
+	}
+	else if (ext == "xml")
+	{
+		mBatchass->getWarpsRef()->loadWarps(mFile);
+	}
+	else if (ext == "patchjson")
+	{
+		// try loading patch
+		try
+		{
+			JsonTree patchjson;
+			try
+			{
+				patchjson = JsonTree(loadFile(mFile));
+				mParameterBag->mCurrentFilePath = mFile;
+			}
+			catch (cinder::JsonTree::Exception exception)
+			{
+				CI_LOG_V("patchjsonparser exception " + mFile + ": " + exception.what());
+
+			}
+			//Assets
+			int i = 1; // 0 is audio
+			JsonTree jsons = patchjson.getChild("assets");
+			for (JsonTree::ConstIter jsonElement = jsons.begin(); jsonElement != jsons.end(); ++jsonElement)
+			{
+				string jsonFileName = jsonElement->getChild("filename").getValue<string>();
+				int channel = jsonElement->getChild("channel").getValue<int>();
+				if (channel < mBatchass->getTexturesRef()->getTextureCount())
+				{
+					CI_LOG_V("asset filename: " + jsonFileName);
+					mBatchass->getTexturesRef()->setTexture(channel, jsonFileName);
+				}
+				i++;
+			}
+
+		}
+		catch (...)
+		{
+			CI_LOG_V("patchjson parsing error: " + mFile);
+		}
+	}
+	else if (ext == "txt")
+	{
+		// try loading shader parts
+		if (mBatchass->getShadersRef()->loadTextFile(mFile))
+		{
+
+		}
+	}
+	else if (ext == "")
+	{
+		// try loading image sequence from dir
+		if (index < 1) index = 1;
+		if (index > 3) index = 3;
+		mBatchass->getTexturesRef()->createFromDir(mFile + "/", index);
+		// or create thumbs from shaders
+		mBatchass->getShadersRef()->createThumbsFromDir(mFile + "/");
+	}
+	/*if (!loaded && ext == "frag")
+	{
+
+	//mShaders->incrementPreviewIndex();
+
+	if (mShaders->loadPixelFrag(mFile))
+	{
+	mParameterBag->controlValues[22] = 1.0f;
+	timeline().apply(&mTimer, 1.0f, 1.0f).finishFn([&]{ save(); });
+	}
+	if (mCodeEditor) mCodeEditor->fileDrop(event);
+	}*/
+	mParameterBag->isUIDirty = true;
+}
+void ReymentaVRApp::saveThumb()
+{
+
+}
 void ReymentaVRApp::draw()
 {
-	/*
-    unsigned int width, height;
-    char txt[256];
 
-    gl::setMatricesWindow(getWindowSize());
-    gl::clear();
-    gl::color(Color(1, 1, 1));
-
-    // Save current global width and height - they will be changed
-    // by receivetexture if the sender changes dimensions
-    width = g_Width;
-    height = g_Height;
-
-    //
-    // Try to receive the texture at the current size 
-    //
-    // NOTE : if ReceiveTexture is called with a framebuffer object bound, 
-    // include the FBO id as an argument so that the binding is restored afterwards
-    // because Spout uses an fbo for intermediate rendering
-    if (bInitialized) {
-        if (spoutreceiver.ReceiveTexture(SenderName, width, height, spoutTexture->getId(), spoutTexture->getTarget())) {
-            //  Width and height are changed for sender change so the local texture has to be resized.
-            if (width != g_Width || height != g_Height) {
-                // The sender dimensions have changed - update the global width and height
-                g_Width = width;
-                g_Height = height;
-                // Update the local texture to receive the new dimensions
-                spoutTexture = gl::Texture::create(g_Width, g_Height);
-                // reset render window
-                setWindowSize(g_Width, g_Height);
-                return; // quit for next round
-            }
-
-            // Otherwise draw the texture and fill the screen
-            gl::draw(spoutTexture, getWindowBounds());
-
-            // Show the user what it is receiving
-            gl::enableAlphaBlending();
-            sprintf_s(txt, "Receiving from [%s]", SenderName);
-            gl::drawString(txt, vec2(toPixels(20), toPixels(20)), Color(1, 1, 1), Font("Verdana", toPixels(24)));
-            sprintf_s(txt, "fps : %2.2d", (int)getAverageFps());
-            gl::drawString(txt, vec2(getWindowWidth() - toPixels(100), toPixels(20)), Color(1, 1, 1), Font("Verdana", toPixels(24)));
-            gl::drawString("RH click to select a sender", vec2(toPixels(20), getWindowHeight() - toPixels(40)), Color(1, 1, 1), Font("Verdana", toPixels(24)));
-            gl::disableAlphaBlending();
-            return; // received OK
-        }
-    }
-
-    gl::enableAlphaBlending();
-    gl::drawString("No sender detected", vec2(toPixels(20), toPixels(20)), Color(1, 1, 1), Font("Verdana", toPixels(24)));
-    gl::disableAlphaBlending();*/
-    // ----------------------------
 }
+void ReymentaVRApp::setUniforms()
+{
+	auto shader = gl::context()->getGlslProg();
+	if (!shader)
+		return;
+
+	// Calculate shader parameters.
+	vec3  iResolution(vec2(getWindowSize()), 1);
+	mParameterBag->iChannelTime[0] = (float)getElapsedSeconds();
+	mParameterBag->iChannelTime[1] = (float)getElapsedSeconds() - 1;
+	mParameterBag->iChannelTime[2] = (float)getElapsedSeconds() - 2;
+	mParameterBag->iChannelTime[3] = (float)getElapsedSeconds() - 3;
+	//
+	if (mParameterBag->mUseTimeWithTempo)
+	{
+		mParameterBag->iGlobalTime = mParameterBag->iTempoTime*mParameterBag->iTimeFactor;
+	}
+	else
+	{
+		mParameterBag->iGlobalTime = (float)getElapsedSeconds();
+	}
+	mParameterBag->iGlobalTime *= mParameterBag->iSpeedMultiplier;
+
+	vec3  iChannelResolution0 = mChannel0 ? vec3(mChannel0->getSize(), 1) : vec3(1);
+	vec3  iChannelResolution1 = mChannel1 ? vec3(mChannel1->getSize(), 1) : vec3(1);
+	vec3  iChannelResolution2 = mChannel2 ? vec3(mChannel2->getSize(), 1) : vec3(1);
+	vec3  iChannelResolution3 = mChannel3 ? vec3(mChannel3->getSize(), 1) : vec3(1);
+
+	time_t now = time(0);
+	tm*    t = gmtime(&now);
+	vec4   iDate(float(t->tm_year + 1900),
+		float(t->tm_mon + 1),
+		float(t->tm_mday),
+		float(t->tm_hour * 3600 + t->tm_min * 60 + t->tm_sec));
+
+	// Set shader uniforms.
+	shader->uniform("iResolution", iResolution);
+	shader->uniform("iGlobalTime", mParameterBag->iGlobalTime);
+	shader->uniform("iChannelTime[0]", mParameterBag->iChannelTime[0]);
+	shader->uniform("iChannelTime[1]", mParameterBag->iChannelTime[1]);
+	shader->uniform("iChannelTime[2]", mParameterBag->iChannelTime[2]);
+	shader->uniform("iChannelTime[3]", mParameterBag->iChannelTime[3]);
+	shader->uniform("iChannelResolution[0]", iChannelResolution0);
+	shader->uniform("iChannelResolution[1]", iChannelResolution1);
+	shader->uniform("iChannelResolution[2]", iChannelResolution2);
+	shader->uniform("iChannelResolution[3]", iChannelResolution3);
+	shader->uniform("iMouse", mMouse);
+	shader->uniform("iChannel0", 0);
+	shader->uniform("iChannel1", 1);
+	shader->uniform("iChannel2", 2);
+	shader->uniform("iChannel3", 3);
+	shader->uniform("iDate", iDate);
+}
+
+#pragma warning(pop) // _CRT_SECURE_NO_WARNINGS
 void prepareSettings(App::Settings *settings)
 {
 	hmd::RiftManager::initialize();
